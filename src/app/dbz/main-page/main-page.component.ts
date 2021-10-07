abd3497627c1d47f0e2867208d2a82fce3cb4a66
Francisco Gallegos <fgallegosgo@gmail.com>
2021-10-07 13:06:27 -0600

    show personages list

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,13 +12,33 @@ interface Personage {
 })
 export class MainPageComponent {
 
+  personages: Personage[] = [
+    {
+      name: "Goku",
+      power: 15000
+    },
+    {
+      name: "Vegeta",
+      power: 9000
+    }
+  ];
+
   new: Personage = {
-    name: 'Trucks',
-    power: 14000
+    name: '',
+    power: 0
   }
 
   add() {
+    if( this.new.name.trim().length === 0 ) { return; }
+    
     console.log( this.new );
+
+    this.personages.push(this.new);
+
+    this.new = {
+      name: "",
+      power: 0
+    }
   }
 
   changeName(event: any) {
